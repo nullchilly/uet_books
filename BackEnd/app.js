d3d039442b2d4e5b0bb0dfc96a489e2c56de9363
juStD-34 <dosangnd2020@gmail.com>
2024-04-27 14:37:33 +0700

    add

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -14,6 +14,7 @@ app.listen(PORT, (error) => {
     );
   else console.log("Error occurred, server can't start", error);
 });
+app.use(cors());
 app.use(express.json());
 app.use("/", authRouter);
 app.use("/user", userRoute);
