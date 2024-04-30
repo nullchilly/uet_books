69a1244964a0c5cc2616124680f9d9341174c3cd
Thành Tấn Trương <64310557+truongtanthanh1202@users.noreply.github.com>
2024-04-30 13:03:08 +0700

    update 2

--- FrontEnd/src/pages/UserPage/SearchPage/index.tsx
+++ FrontEnd/src/pages/UserPage/SearchPage/index.tsx
@@ -5,16 +5,7 @@ import BookCardList from "./BookCardList";
 
 const SearchPage = () => {
   return (
-    <Box
-      id="style-2"
-      sx={{
-        backgroundColor: "#f3f3f7",
-        width: "calc(100% - var(--default-layout-width-sidebar))",
-        height: "calc(100vh - var(--default-layout-height-header))",
-        float: "right",
-        overflowY: "scroll",
-      }}
-    >
+    <Box id="style-2" className="container">
       <Box className="BookCardList-container">
         <BookCardList />
         <BookCardList />
